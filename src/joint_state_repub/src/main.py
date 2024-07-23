9ee7029fdd9502bb7887ef49123d2aea6d57a8a6
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2024-07-23 12:00:16 +1000

    export hand joint velocity

--- src/joint_state_repub/src/main.py
+++ src/joint_state_repub/src/main.py
@@ -9,12 +9,15 @@ from std_msgs.msg import Float64
 class JointPositionRepublisher:
     def joint_state_cb(self, msg: JointState):
         self.joints = len(msg.position)
-        for i, pub in enumerate(self.joint_pub):
+        for i, pub in enumerate(self.joint_pos_pub):
             pub.publish(Float64(msg.position[i]))
+        for i, pub in enumerate(self.joint_vel_pub):
+            pub.publish(Float64(msg.velocity[i]))
 
     def __init__(self, topic='/joint_state'):
         self.joints = 0 # number of joints
-        self.joint_pub = [] # list of joint (re)publishers
+        self.joint_pos_pub = [] # list of joint position (re)publishers
+        self.joint_vel_pub = [] # and for velocity too
 
         rospy.loginfo(f'setting up subscriber ({topic})')
         self.joint_state = rospy.Subscriber(topic, JointState, self.joint_state_cb)
@@ -24,12 +27,13 @@ class JointPositionRepublisher:
 
         rospy.loginfo('creating joint republishers')
         for i in range(self.joints):
-            self.joint_pub.append(rospy.Publisher(f'{topic}_pos/{i}', Float64, queue_size=10))
+            self.joint_pos_pub.append(rospy.Publisher(f'{topic}_pos/{i}', Float64, queue_size=10))
+            self.joint_vel_pub.append(rospy.Publisher(f'{topic}_vel/{i}', Float64, queue_size=10))
 
         rospy.loginfo('ready')
 
 if __name__ == '__main__':
-    rospy.init_node('joint_pos_repub')
+    rospy.init_node('joint_state_repub')
     # rospy.loginfo('Hello, World!')
     
     JointPositionRepublisher('/allegroHand_0/joint_states')
