adeaa6fb1fe403fbd9336d7952b65e6f565feecc
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2024-07-30 15:28:24 +1000

    added more hand teleop commands

--- src/hand_teleop/src/main.py
+++ src/hand_teleop/src/main.py
@@ -43,38 +43,49 @@ class _GetchWindows:
 getch = _Getch()
 
 BINDINGS = {
-    '1': (0, 0.1),
-    'q': (0, -0.1),
-    '2': (1, 0.1),
-    'w': (1, -0.1),
-    '3': (2, 0.1),
-    'e': (2, -0.1),
-    '4': (3, 0.1),
-    'r': (3, -0.1),
-    '5': (4, 0.1),
-    't': (4, -0.1),
-    '6': (5, 0.1),
-    'y': (5, -0.1),
-    '7': (6, 0.1),
-    'u': (6, -0.1),
-    '8': (7, 0.1),
-    'i': (7, -0.1),
-    'a': (8, 0.1),
-    'z': (8, -0.1),
-    's': (9, 0.1),
-    'x': (9, -0.1),
-    'd': (10, 0.1),
-    'c': (10, -0.1),
-    'f': (11, 0.1),
-    'v': (11, -0.1),
-    'g': (12, 0.1),
-    'b': (12, -0.1),
-    'h': (13, 0.1),
-    'n': (13, -0.1),
-    'j': (14, 0.1),
-    'm': (14, -0.1),
-    'k': (15, 0.1),
-    ',': (15, -0.1)
+    '1': ([0], 0.1),
+    'q': ([0], -0.1),
+    '2': ([1], 0.1),
+    'w': ([1], -0.1),
+    '3': ([2], 0.1),
+    'e': ([2], -0.1),
+    '4': ([3], 0.1),
+    'r': ([3], -0.1),
+    '5': ([4], 0.1),
+    't': ([4], -0.1),
+    '6': ([5], 0.1),
+    'y': ([5], -0.1),
+    '7': ([6], 0.1),
+    'u': ([6], -0.1),
+    '8': ([7], 0.1),
+    'i': ([7], -0.1),
+    'a': ([8], 0.1),
+    'z': ([8], -0.1),
+    's': ([9], 0.1),
+    'x': ([9], -0.1),
+    'd': ([10], 0.1),
+    'c': ([10], -0.1),
+    'f': ([11], 0.1),
+    'v': ([11], -0.1),
+    'g': ([12], 0.1),
+    'b': ([12], -0.1),
+    'h': ([13], 0.1),
+    'n': ([13], -0.1),
+    'j': ([14], 0.1),
+    'm': ([14], -0.1),
+    'k': ([15], 0.1),
+    ',': ([15], -0.1),
+    '9': ([1, 5, 9, 14], 0.1),
+    'o': ([1, 5, 9, 14], -0.1),
+    '(': ([1, 5, 9], 0.1),
+    'O': ([1, 5, 9], -0.1),
+    '0': ([2, 6, 10], 0.1),
+    'p': ([2, 6, 10], -0.1),
+    '-': ([3, 7, 11, 15], 0.1),
+    '[': ([3, 7, 11, 15], -0.1),
+    '_': ([3, 7, 11], 0.1),
+    '{': ([3, 7, 11], -0.1)
+    
 }
 
 class HandTeleop:
@@ -92,22 +103,34 @@ class HandTeleop:
         self.hand_update = False
 
         rospy.loginfo(f'press Space to read hand position, / to toggle continuous updating (default: {self.hand_update})')
+        rospy.loginfo(f'press \' to set hand position')
         rospy.loginfo(f'press \\ to quit')
 
         while not rospy.is_shutdown():
-            c = getch().lower()
+            c = getch()
             if c == ' ':
                 rospy.loginfo('position: ' + ','.join([str(x) for x in self.hand_pos]))
             elif c == '/':
                 self.hand_update = not self.hand_update
                 rospy.loginfo(f'continuous update set to {self.hand_update}')
+            elif c == '\'':
+                newpos = input('enter your desired joint position (or enter nothing to set to current):')
+                if len(newpos) > 0: self.hand_pos = [float(pos) for pos in newpos.split(',')]
+                self.hand_pub.publish(JointState(
+                    Header(0, rospy.Time.now(), 'nul'),
+                    '',
+                    self.hand_pos, # send position only
+                    [], [] # disregard velocity and effort
+                ))
+                rospy.loginfo(f'set hand position to ' + ','.join([str(x) for x in self.hand_pos]))
             elif c == '\\': break
             elif c in BINDINGS:
-                joint, delta = BINDINGS[c]
+                joints, delta = BINDINGS[c]
                 if self.hand_update: pos = list(self.hand_pos) # copy out for atomicity
                 else: pos = self.hand_pos # by reference
-                pos[joint] += delta
-                rospy.loginfo(f'joint {joint} -> {pos[joint]}')
+                for joint in joints:
+                    pos[joint] += delta
+                rospy.loginfo(f'joint {joints} -> {[pos[joint] for joint in joints]}')
                 self.hand_pub.publish(JointState(
                     Header(0, rospy.Time.now(), 'nul'),
                     '',
