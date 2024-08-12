15473a89990c4722d4e4172cb0b349aee1cf7d96
Thanh Vinh Nguyen <ngtv0404@gmail.com>
2024-08-12 10:41:44 +1000

    adjustments for 12 Aug demo

--- src/arm_controller/src/xarm.py
+++ src/arm_controller/src/xarm.py
@@ -9,8 +9,8 @@ from arm_controller.srv import *
 from arm_controller.msg import *
 
 ARM_HOME = [-1.5707963705062866, -0.6283185482025146, 0.0, 0.0, 3.1415927410125732, 0.9599310755729675, 1.8168877363204956] # initial arm joint angles
-ARM_JOINT_MAXVEL = 0.7 # rad/s
-ARM_JOINT_MAXACC = 14.0 # rad/s^2
+ARM_JOINT_MAXVEL = 0.4 # rad/s
+ARM_JOINT_MAXACC = 7.5 # rad/s^2
 ARM_MOVE_MAXVEL = 400.0 # mm/s
 ARM_MOVE_MAXACC = 4000.0 # mm/s^2
 
